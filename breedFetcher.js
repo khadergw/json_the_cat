b4daf10605faf112e4f0389bdf73b3667a63674e
GHadeer Khader <qods_a@yahoo.com>
2021-12-02 04:28:30 +0000

    updates

---
+++ breedFetcher.js
@@ -0,0 +1,29 @@
+const request = require('request');
+
+const fetchBreedDescription = function(breedName, callback) {
+
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (body) => {
+   console.log(body);
+   console.log(typeof body);
+  })
+}
+fetchBreedDescription('Siberian');
+//     if (error) {
+//       callback(`Failed to request details: ${error}`,null);
+//     }
+
+//     const data = JSON.parse(body);
+//     // console.log(data);
+
+//     const breed = data[0];
+//     if (breed) {
+//       callback(null,breed.description);
+//     } else {
+//       callback(`Failed to find breed ${breedName}`,null);
+//     }
+//   });
+
+// };
+
+// module.exports = { fetchBreedDescription };
